1d020e078bb661d7d4a324d7e40a3a017acec7aa
Marc Gravell <marc.gravell@gmail.com>
2016-10-06 13:22:39 +0100

    Huffman encode should work on the stack; don't use the indexer - use limited .Write commands

--- src/Channels.Http2/HuffmanCode.cs
+++ src/Channels.Http2/HuffmanCode.cs
@@ -87,29 +87,36 @@ namespace Channels.Http2
             public bool IsLeaf => True == null;
         }
 
-        internal static void Write(WritableBuffer buffer, string value)
+        internal static unsafe void Write(WritableBuffer buffer, string value)
         {
-            var span = default(Span<byte>);
-            int unusedBytesInSpan = 0, dirtyBytes = 0, index = -1;
             int charLen = value.Length;
-            int bitsLeftInCurrentByte = 0;
+            if (charLen == 0) return; // nothing to do
+
+            // we know we're going to need at least one byte, so init as though
+            // we are about to write that first byte with 8 bits available
+            const int StackSize = 512;
+            byte* scratch = stackalloc byte[StackSize], writeHead = scratch;
+            int unusedBytes = StackSize - 1, dirtyBytes = 1, bitsLeftInCurrentByte = 8;
+
             for (int charIndex = 0; charIndex < charLen; charIndex++)
             {
                 // going to need at least *something*
                 if (bitsLeftInCurrentByte == 0)
                 {
-                    if (unusedBytesInSpan != 0)
-                    {   // manually inline the "simple" case
-                        index++;
+                    if (unusedBytes-- != 0)
+                    {
                         dirtyBytes++;
-                        unusedBytesInSpan--;
-                        bitsLeftInCurrentByte = 8;
-                        span[index] = 0;
+                        *(++writeHead) = 0;
                     }
                     else
-                    {   // going to need a new memory block
-                        NewByte(ref buffer, ref span, ref unusedBytesInSpan, ref dirtyBytes, ref index, out bitsLeftInCurrentByte);
+                    {
+                        // flush
+                        buffer.Write(new Span<byte>(scratch, dirtyBytes));
+                        writeHead = scratch;
+                        dirtyBytes = 1;
+                        unusedBytes = StackSize - 1;
                     }
+                    bitsLeftInCurrentByte = 8;
                 }
 
                 byte c = (byte)value[charIndex];
@@ -121,7 +128,7 @@ namespace Channels.Http2
                     int shift = bitsLeftInCurrentByte - (int)bitsForCode;
                     var shiftedCode = (byte)(_codes[c] << shift);
 
-                    span[index] |= shiftedCode;
+                    *writeHead |= shiftedCode;
                     bitsLeftInCurrentByte -= bitsForCode;
                 }
                 else
@@ -131,23 +138,25 @@ namespace Channels.Http2
                     int shift = bitsForCode - bitsLeftInCurrentByte;
 
                     // write the first byte (note we know the shift will nuke all but the needed bits)
-                    span[index] |= (byte)(code >> shift);
+                    *writeHead |= (byte)(code >> shift);
                     shift -= 8;
 
                     while (shift >= 8)
                     {
-                        // write any complete bytes
-                        if (unusedBytesInSpan != 0)
-                        {   // manually inline the "simple" case
-                            index++;
+                        if (unusedBytes-- != 0)
+                        {
                             dirtyBytes++;
-                            unusedBytesInSpan--;
+                            writeHead++;
                         }
                         else
-                        {   // going to need a new memory block
-                            NewByte(ref buffer, ref span, ref unusedBytesInSpan, ref dirtyBytes, ref index, out bitsLeftInCurrentByte);
+                        {
+                            // flush
+                            buffer.Write(new Span<byte>(scratch, dirtyBytes));
+                            writeHead = scratch;
+                            dirtyBytes = 1;
+                            unusedBytes = StackSize - 1;
                         }
-                        span[index] = (byte)((code >> shift) & 0xFF);
+                        *writeHead = (byte)((code >> shift) & 0xFF);
                         shift -= 8;
                     }
 
@@ -155,19 +164,22 @@ namespace Channels.Http2
                     if (shift != 0)
                     {
                         shift &= 0x07; // can be negative
-                        if (unusedBytesInSpan != 0)
-                        {   // manually inline the "simple" case
-                            index++;
+                        if (unusedBytes-- != 0)
+                        {
                             dirtyBytes++;
-                            unusedBytesInSpan--;
+                            writeHead++;
                         }
                         else
-                        {   // going to need a new memory block
-                            NewByte(ref buffer, ref span, ref unusedBytesInSpan, ref dirtyBytes, ref index, out bitsLeftInCurrentByte);
+                        {
+                            // flush
+                            buffer.Write(new Span<byte>(scratch, dirtyBytes));
+                            writeHead = scratch;
+                            dirtyBytes = 1;
+                            unusedBytes = StackSize - 1;
                         }
                         bitsLeftInCurrentByte = 8 - shift;
                         var mask = ~(~0 << shift);
-                        span[index] = (byte)((code & mask) << bitsLeftInCurrentByte);
+                        *writeHead = (byte)((code & mask) << bitsLeftInCurrentByte);
                     }
                     else
                     {
@@ -175,39 +187,15 @@ namespace Channels.Http2
                     }
                 }
             }
-            if(bitsLeftInCurrentByte != 0)
+            if (bitsLeftInCurrentByte != 0)
             {
                 // pad with 1
-                span[index] |= (byte)(~(~0 << bitsLeftInCurrentByte));
+                *writeHead |= (byte)(~(~0 << bitsLeftInCurrentByte));
             }
             if (dirtyBytes != 0)
             {
-                buffer.Advance(dirtyBytes);
+                buffer.Write(new Span<byte>(scratch, dirtyBytes));
             }
-
-
-        }
-
-        private static void NewByte(ref WritableBuffer buffer, ref Span<byte> span, ref int unusedBytesInSpan, ref int dirtyBytes, ref int index,
-            out int bitsLeftInCurrentByte)
-        {
-            if (unusedBytesInSpan == 0)
-            {
-                if (dirtyBytes != 0) { buffer.Advance(dirtyBytes); }
-                buffer.Ensure();
-                span = buffer.Memory.Span;
-                unusedBytesInSpan = span.Length - 1;
-                dirtyBytes = 1;
-                index = 0;
-            }
-            else
-            {
-                dirtyBytes++;
-                unusedBytesInSpan--;
-                index++;
-            }
-            span[index] = 0; // we're going to use a lot of |; ensure we have 0 to start with!
-            bitsLeftInCurrentByte = 8;
         }
 
         internal static int GetByteCount(string value)
